f9e8870b904981a0ed488955d3f8bd0a8b750efb
BIueMan <55624377+BIueMan@users.noreply.github.com>
2020-01-07 11:45:30 +0200

    Update Employee.cpp

--- HW4/Employee.cpp
+++ HW4/Employee.cpp
@@ -1,1 +1,42 @@
-//#include "Employee.h"
+#include "Employee.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Employee::Employee(char* name, int payPerHoure, char** workHours) :
+					name_(name), payPerHoure_(payPerHoure), workHours_(workHours) {}
+
+char* Employee::getName()
+{
+	return name_;
+}
+
+int Employee::getSalary()
+{
+	return payPerHoure_;
+}
+
+int Employee::calcWeeklySalary()
+{
+	int sumHoure = 0;
+
+	// get all work hours
+	for (int i = 0; i < DAYS_IN_WEEK; i++)
+	{
+		char startWork[3], endWork[3];
+		char* dayWork = workHours_[i]; 
+
+		startWork[0] = dayWork[0];
+		startWork[1] = dayWork[1];
+		startWork[2] = '\0';
+
+		endWork[0] = dayWork[3];
+		endWork[1] = dayWork[4];
+		endWork[2] = '\0';
+
+		//add the work hours of the day
+		sumHoure += (atoi(endWork) - atoi(startWork));
+	}
+
+	return sumHoure * payPerHoure_;
+}
